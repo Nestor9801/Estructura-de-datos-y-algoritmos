8c0858ead07cfc5d0ae7345f9b7a28554e04abf4
Nestor Osiel Zavaleta Barrales <54288868+Nestor9801@users.noreply.github.com>
2021-07-27 14:56:03 -0500

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,9 +0,0 @@
-from list_manager import get_my_list
-
-lista_ = ['hola', 1,2,3,4,5, [34,5,6], {"1":1,"2":2}, 8.0,9.0, True, 7 + 6j]
-
-
-extraccion_lista = get_my_list(7)
-print(extraccion_lista)
-
-
